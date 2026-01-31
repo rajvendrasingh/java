9a98e5a3fc1dc8caeb28ff017137eb42bf8868b9
rajvendrasingh <sseema7350@gmail.com>
2026-01-31 21:52:06 +0530

    Add files via upload

---
+++ Day1.java
@@ -0,0 +1,28 @@
+public class Day1 {
+    public static void main(String args[]){
+        System.out.print("hello raj");
+    }
+}
+
+//how is my code run?
+/*(i) compilation   (source code ->byte code)
+(ii) execution (byte code -> machine code (native code))*/
+// jvm (java virtual machine)  -> jre (java runtime environment )  -> jdk(java development kit)
+
+
+// 1.function     2.class
+
+
+// java is a programing language
+// with the help of java we can crate sowftware application , anroid app, web application etc. 
+// java is a platform independent lnnguage
+
+// java code -> byte code -> jvm -> output
+// always class name and file name should be same 
+// start program public static void main.
+// for printio output use System.out.print()
+
+// basic rules in java.
+// 1. file name = class name
+//2. java is case sensitive language (h!=H)
+//3. every statement end with ;
