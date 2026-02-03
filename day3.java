b527ad07ca330673de70ff583db4271871363bc5
rajvendrasingh <sseema7350@gmail.com>
2026-02-03 19:56:28 +0530

    Add files via upload

---
+++ day3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class day3 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter your age:");
+        int age = sc.nextInt();
+        if (age >=18){
+            System.out.println("you are adult");
+            
+        }
+        else{
+            System.out.println("you are not adult");
+        }
+    sc.close();
+}}
+// conditional statements in java 
+// 1. if,else if, else
+    //2. switch case 
+///3. break
+/// if (condtion){
+///  }
+/// else{
+/// }
