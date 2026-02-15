55ce07fe714728060ea37d032a630735a6b513ec
rajvendrasingh <sseema7350@gmail.com>
2026-02-15 20:44:52 +0530

    Add files via upload

---
+++ Day5.java
@@ -0,0 +1,13 @@
+public class Day5 {
+    public static void main(String args[]) {
+        // int n=4;
+        // int m=5;  // nested loop to print the pattern of stars
+        for (int i=1; i<=5; i++){
+            for (int j=1; j<=5; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+    
+}}
