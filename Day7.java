87861110b2b1044cfa906cbddb850626859cd04a
rajvendrasingh <sseema7350@gmail.com>
2026-02-17 22:58:21 +0530

    Update Day7.java

--- Day7.java
+++ Day7.java
@@ -1,3 +1,35 @@
-public class Day7 {
+public class Day7 { 
+    public static void main (String[]args){
+        // int num =1;
+        // int n=5;
+
+        // // outer loop
+        // for(int i=1; i<=n; i++){
+        //     //inner loop
+        //     for (int j=1; j<=i; j++){
+        //          System.out.print(num + " ");
+        //         num++;// num = num+1
+                
+        //     }
+        //     System.out.println();
+        // }
+        int n=5;
+        //outer loop
+        for ( int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                int sum=i+j;
+                if(sum%2==0){
+                    System.out.print("1"+" ");
+                }
+                else{
+                    System.out.print("0"+ " ");
+                }
+
+            }
+            System.out.println();
+
+        }
+    }
     
 }
+
