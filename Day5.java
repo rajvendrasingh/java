79c6d5fa0ad28d48f2ba490fcc3d4f3964d9aa7a
rajvendrasingh <sseema7350@gmail.com>
2026-02-17 22:22:31 +0530

    Fix nested loop for star pattern printing

--- Day5.java
+++ Day5.java
@@ -7,7 +7,42 @@ public class Day5 {
                 System.out.print("*");
             }
             System.out.println();
+        }// outer loop
+        // int n=4;
+        // int m=5;  // nested loop to print the pattern of stars
+        // for (int i=1; i<=n; i++){
+        //     // iner loop
+        //     for (int j=1; j<=m; j++){
+        //         // cell -> (i,j)
+        //         if (i==1 || j==1 || i==n || j==m){
+        //             System.out.print("*");
+
+        //         } else {
+        //             System.out.print(" ");
+        //         }
+                
+
+        //     }
+        //     System.out.println();
+        // }
+
+    //    int n=4;
+     
+    //  for (int i=n; i>=1; i--){// outer loop
+    //     for (int j=1; j<=i; j++){  // ineer loop
+    //         System.out.print("*");
+    //     }
+    //     System.out.println();
+    //  }
+
+    int m=4;
+    for (int i=m; i<=1; i--){
+        for (int j=1; j<=i; j++){
+            System.out.print("*");
         }
+        System.out.println();
+    }
         
     
 }}
+
