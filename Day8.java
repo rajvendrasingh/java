7a13924e5d908add4888f94d2db8a406ea567df1
rajvendrasingh <sseema7350@gmail.com>
2026-02-18 23:29:40 +0530

    Add files via upload

---
+++ Day8.java
@@ -0,0 +1,60 @@
+public class Day8 {
+    public static void main(String[] args){
+    //     int n=4;
+    //     //upper half
+    //     for (int i=1; i<=n; i++){
+    //         //1st part
+    //         for(int j=1; j<=i; j++){
+    //             System.out.print("*" );
+                
+    //         }
+    //         // spaces
+    //         int spaces=2*(n-i);
+    //          for(int j=1; j<=spaces; j++){
+    //             System.out.print( " ");
+    //          }
+    //          //2nd part
+    //           for(int j=1; j<=i; j++){
+    //             System.out.print("*");
+    //         }
+    //         System.out.println();
+            
+            
+    // }
+    // // lower half
+    
+        
+    //     for (int i=n; i>=1; i--){
+    //         //1st part
+    //         for(int j=1; j<=i; j++){
+    //             System.out.print("*" );
+                
+    //         }
+    //         // spaces
+    //         int spaces=2*(n-i);
+    //          for(int j=1; j<=spaces; j++){
+    //             System.out.print( " ");
+    //          }
+    //          //2nd part
+    //           for(int j=1; j<=i; j++){
+    //             System.out.print( "*");
+    //         }
+    //         System.out.println();
+            
+            
+    // }
+    int n=5;
+    
+    for(int i=1; i<=n; i++){
+     // for spaces 
+     for(int j=1; j<=n-i; j++){
+        System.out.print(" ");
+     }
+     for(int j=1; j<=n; j++){
+        System.out.print("*");
+     }
+     System.out.println();
+    }
+    
+
+}}
