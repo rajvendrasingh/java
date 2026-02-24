10809d45720542bc60f3a62b793c6d2539f88054
rajvendrasingh <sseema7350@gmail.com>
2026-02-24 23:15:05 +0530

    Update Day9.java

--- Day9.java
+++ Day9.java
@@ -1,21 +1,21 @@
 public class Day9 {
-    public void main (String[]args){
+    public Static void main (String[]args){
 
     int n=5;
-    for (int i=1; i<=5; i++){
+    for (int i=1; i<=n; i++){
         // spaces 
-        for (int j=1; j<=n-1;){
+        for (int j=1; j<=n-i; j++){
             System.out.print(" ");
         }
     
     
 // for num
     for (int j=1; j<=i; j++){
-        System.out.print(i);
-    
-    
-
+        System.out.print(i +" ");
     }
     System.out.println();
     
-}}}
+}
+    }
+}
+
