545da667e845651b7499f78f02ebd52d8d984ba1
rajvendrasingh <sseema7350@gmail.com>
2026-02-24 23:54:43 +0530

    Implement palindromic pattern printing in Day9
    Added a palindromic pattern print after the original pattern.

--- Day9.java
+++ Day9.java
@@ -16,6 +16,22 @@ public class Day9 {
     System.out.println();
     
 }
+         // palindromic pattern print in java
+        int n=5;
+        for (int i=1; i<=n; i++){
+            for (int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            for (int j=i; j>=1; j--){
+                System.out.print(j);
+            }
+            for (int j=2; j<=i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+          
+        }
     }
 }
+
 
