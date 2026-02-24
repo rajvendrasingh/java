b2aa56aa2d9a73af726dfa43ab6e09ee438308e6
rajvendrasingh <sseema7350@gmail.com>
2026-02-24 22:52:15 +0530

    Add files via upload

--- Day9.java
+++ Day9.java
@@ -1,3 +1,21 @@
 public class Day9 {
+    public void main (String[]args){
+
+    int n=5;
+    for (int i=1; i<=5; i++){
+        // spaces 
+        for (int j=1; j<=n-1;){
+            System.out.print(" ");
+        }
     
-}
+    
+// for num
+    for (int j=1; j<=i; j++){
+        System.out.print(i);
+    
+    
+
+    }
+    System.out.println();
+    
+}}}
