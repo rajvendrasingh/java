ee22e1d25b20b50260e8065304bdb3fee254a68b
rajvendrasingh <sseema7350@gmail.com>
2026-06-16 22:25:47 +0530

    Add files via upload

---
+++ SummerDay10.java
@@ -0,0 +1,19 @@
+public class SummerDay10 {
+    // recursion in javam
+    public static void main(String[] args) {
+        // print 5 to 1 for use iterration
+    //    int n=5 ;
+    //     for (int i = n ; i>0; i--){
+    //         System.out.println(i);
+    //     }
+
+    // print n to 5 with recursion
+    public static void printnum(int n){
+        
+    }
+    
+
+        
+    }
+    
+}
